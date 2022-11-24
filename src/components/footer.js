6760bae8bfd72c1e0a3bdb812554696c4fac7fd5
a14a19 <anas31197@gmail.com>
2022-11-24 18:29:46 +0530

    Movie-search-app

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,16 +8,16 @@ function Footer() {
                 Anas Ahmed
             </a>
             <div className={classes.social}>
-                <a href='https://www.linkedin.com/in/anas-ahmed-3b927913a/' target='_blank' rel='noreferrer'>
+                <a href='https://www.linkedin.com/in/anas-ahmed-3b927913a/' target='_blank' rel='noreferrer' title='Connect on LinkedIn'>
                     <i class="fa-brands fa-linkedin"></i>
                 </a>
                 <a href='https://github.com/a14a19/movie-search-app.git' title='View code on Github' target='_blank' rel='noreferrer'>
                     <i class="fa-brands fa-github"></i>
                 </a>
-                <a href='mailto:anas31197@gmail.com' target='_blank' rel='noreferrer'>
+                <a href='mailto:anas31197@gmail.com' target='_blank' rel='noreferrer' title='Email me'>
                     <i class="fa-solid fa-envelope"></i>
                 </a>
-                <a href='https://wa.me/+18507248774' target='_blank' rel='noreferrer'>
+                <a href='https://wa.me/+18507248774' target='_blank' rel='noreferrer' title='Connect on Whatsapp'>
                     <i class="fa-brands fa-whatsapp"></i>
                 </a>
             </div>
